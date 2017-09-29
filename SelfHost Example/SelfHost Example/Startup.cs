23c41716540dbd8f9caf677f856a30461d8bac1a
Gopi <pgopirmd@gmail.com>
2017-09-29 20:32:33 +0530

    refactorize

--- SelfHost Example/SelfHost Example/Startup.cs
+++ SelfHost Example/SelfHost Example/Startup.cs
@@ -1,16 +1,35 @@
 ﻿using System.Web.Http;
 using Owin;
 using System.Web.Http.Cors;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using System;
+//using Microsoft.AspNet.Identity.Owin;
+//using Microsoft.Owin.Security;
+//using Microsoft.Owin.Security.Cookies;
+
 
 namespace GuviCRMSuite
 {
     public class Startup
     {
+        static string PublicClientId = null;
+        static OAuthAuthorizationServerOptions OAuthOptions = null;
         public void Configuration(IAppBuilder app)
         {
             // First Endpoint
             var config = new HttpConfiguration();
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            
+            //for token based authentication
+            //PublicClientId = "self";
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions {
+                TokenEndpointPath = new Microsoft.Owin.PathString("/token"),
+                Provider = new OAuthAuthorizationServerProvider(),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                AllowInsecureHttp = true
+            });
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
 
             //Login route
             config.Routes.MapHttpRoute(
